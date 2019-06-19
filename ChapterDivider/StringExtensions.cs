69c5e02fa91cfcd57634f7112755885e1549586a
Reiss Florian <85802@supinfo.com>
2019-06-19 19:35:43 +0200

    Added extension to remove html tags from a string.

---
+++ ChapterDivider/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ChapterDivider
+{
+    internal static class StringExtensions
+    {
+        public static string StripHTML(this string input)
+        {
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+    }
+}
